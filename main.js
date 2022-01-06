044b17ed8d64ea940ee5d4d06ae75cc00ff2d613
Ronald Head <ronaldlhead@gmail.com>
2022-01-05 17:08:24 -0700

    Add functions to capture title and body for idea card

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 var list = [];
+
+var titleValue = document.getElementById('title-value');
+var bodyValue = document.getElementById('body-value');
+var saveButton = document.getElementById('save-button');
+
+saveButton.addEventListener('click', saveIdea);
+
+function saveIdea() {
+  event.preventDefault();
+  
+  var newTitle = titleValue.value;
+  var newBody = bodyValue.value;
+  var newIdea = new Idea(newTitle, newBody);
+
+  list.push(newIdea);
+  return newIdea;
+}
+
+function displayNewIdea() {
+
+}
